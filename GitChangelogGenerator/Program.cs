d68fe7f61f442930b28b7732d3be014285330850
mzwierzykowski <zwierzykowski.m@gmail.com>
2018-10-26 16:35:20 +0200

    Wersja konsolowa

--- GitChangelogGenerator/Program.cs
+++ GitChangelogGenerator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -7,15 +8,55 @@ namespace GitChangelogGenerator
 {
     class Program
     {
-        const string path = @"D:\Projekty\GitChangelogTest";
+        //const string path = @"D:\Projekty\GitChangelogTest";
         static void Main(string[] args)
         {
-            string rawChangelog = GetRawChangelog(path);
-            var commitList = ParseRawChangelog(rawChangelog);
-            WriteToFile(commitList);
+            var app = new CommandLineApplication();
+            app.Name = "GitChangelog";
+            app.HelpOption("-?|-h|--help");
+
+            var pathArg = app.Option("-p|--path",
+            "Ścieżka do repozytorium git (wymagane)", CommandOptionType.SingleValue);
+
+            var branchArg = app.Option("-b|--branch",
+            "Branch źródłowy dla changelog (opcjonalne)", CommandOptionType.SingleValue);
+
+            var tagArg = app.Option("-t|--tag",
+            "Tag źródłowy lub zakres tag1..tag2 (opcjonalne)", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                string gitCommand = CreateInputCommand(pathArg.Value(), branchArg.Value(), tagArg.Value());
+                string rawChangelog = GetRawChangelog(gitCommand);
+                var commitList = ParseRawChangelog(rawChangelog);
+                WriteToFile(commitList, pathArg.Value());
+                return 0;
+            });
+
+            app.Execute(args);
         }
 
-        private static void WriteToFile(List<Commit> commitList)
+        private static string CreateInputCommand(string pathArg, string branchArg, string tagArg)
+        {
+            if(pathArg == null)
+            {
+                throw new Exception("Brak podanej ścieżki do repozytorium git");
+            }
+            string command = $"--git-dir={pathArg.Replace("\\", "/")}/.git --work-tree={pathArg.Replace("\\", "/")} log ";
+            if (tagArg != null)
+            {
+                command += tagArg;
+            }
+            else if (branchArg != null)
+            {
+                command += branchArg;
+            }
+
+            command += " --format=\"%at>>%s\" --no-merges";
+            return command;
+        }
+
+        private static void WriteToFile(List<Commit> commitList, string path)
         {
             var commitGroups = commitList.GroupBy(x => x.Category).ToDictionary(g => g.Key, g => g.ToList());
             string output = string.Empty;
@@ -23,23 +64,23 @@ namespace GitChangelogGenerator
             {
                 if(group.Key == "new")
                 {
-                    output += "Nowości:\n";
+                    output += "# Nowości\n";
                 }
                 else if (group.Key == "chg")
                 {
-                    output += "Zmiany:\n";
+                    output += "# Zmiany\n";
                 }
                 else if (group.Key == "fix")
                 {
-                    output += "Poprawki:\n";
+                    output += "# Poprawki\n";
                 }
                 foreach (var commit in group.Value)
                 {
-                    output += $"{commit.Date.ToString()} | {commit.Body}\n";
+                    output += $"* [{commit.Date.ToString()}] {commit.Body}\n";
                 }
                 output += "\n";
             }
-            System.IO.File.WriteAllText(path + @"\changelog.txt", output);
+            System.IO.File.WriteAllText(path + @"\changelog.md", output);
         }
 
         private static List<Commit> ParseRawChangelog(string rawChangelog)
@@ -52,10 +93,15 @@ namespace GitChangelogGenerator
                 {
                     var com = new Commit();
                     string[] parts = line.Split(">>");
-                    com.Date = UnixTimestampToDateTime(long.Parse(parts[0]));
-                    com.Category = parts[1].Substring(0, 3);
-                    com.Body = parts[1].Substring(5);
-                    commitList.Add(com);
+                    if (parts[1].Substring(0, 3).Equals("new", StringComparison.InvariantCulture) 
+                        || parts[1].Substring(0, 3).Equals("fix", StringComparison.InvariantCulture)
+                        || parts[1].Substring(0, 3).Equals("chg", StringComparison.InvariantCulture))
+                    {
+                        com.Date = UnixTimestampToDateTime(long.Parse(parts[0]));
+                        com.Category = parts[1].Substring(0, 3);
+                        com.Body = parts[1].Substring(5);
+                        commitList.Add(com);
+                    }
                 }
             }
             return commitList;
@@ -68,9 +114,9 @@ namespace GitChangelogGenerator
             return new DateTime(unixStart.Ticks + unixTimeStampInTicks, System.DateTimeKind.Utc);
         }
 
-        public static string GetRawChangelog(string path)
+        public static string GetRawChangelog(string command)
         {
-            var output = RunProcess(string.Format("--git-dir={0}/.git --work-tree={1} log --format=\"%at>>%s\"", path.Replace("\\", "/"), path.Replace("\\", "/")));
+            var output = RunProcess(command);
             return output;
         }
 
